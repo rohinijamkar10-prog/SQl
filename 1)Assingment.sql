91962c77379fdd9ee271d2459ce22930ee272739
rohinijamkar10-prog <rohinijamkar10@gmail.com>
2026-06-10 18:31:04 +0530

    Add files via upload

---
+++ 1)Assingment.sql
@@ -0,0 +1,22 @@
+create database music_streaming_db;
+create database food_delivery_db;
+(#List 3 differences between MySQL and PostgreSQL in terms of 
+#features or use cases, and give one example of a popular app or company that uses each.)
+# MySQL
+#Performance
+#MySQL is generally faster for simple web applications and read-heavy workloads.
+#PostgreSQL performs better for complex queries and large-scale data processing.
+#Advanced Features
+#MySQL provides basic database features and is easier for beginners.
+#PostgreSQL offers advanced features such as JSONB support, window functions, and custom data types.
+#SQL Standards Compliance
+#MySQL follows SQL standards but with some limitations.
+#PostgreSQL has stronger compliance with SQL standards and supports more advanced SQL features.
+(#MySQL:
+#Facebook
+#YouTube
+#WordPress
+#PostgreSQL:
+#Instagram
+#Reddit
+#Spotify)
